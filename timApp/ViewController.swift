dc56c34b2fbbd5afa46fb8a687abf240023a7585
Kyle <32943027+kylegiltan@users.noreply.github.com>
2020-07-08 15:07:17 -0700

    Initial Commit

---
+++ timApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timApp
+//
+//  Created by Kyle Gil Tan on 7/8/20.
+//  Copyright © 2020 Kyle Gil Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
